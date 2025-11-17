dde596275283001cd00799fed4cbe9adee99897e
Danielle Sinay <danielle.sinay@post.runi.ac.il>
2025-11-18 00:37:59 +0200

    Completed Assigment.1

--- Algebra.java
+++ Algebra.java
@@ -6,18 +6,18 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+	    System.out.println(plus(2,3));   		// 2 + 3
+	    System.out.println(minus(7,2));  		// 7 - 2
+   		System.out.println(minus(2,7));  		// 2 - 7
+ 		System.out.println(times(3,4));  		// 3 * 4
+   		System.out.println(plus(2,times(4,2))); // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+   		System.out.println(div(12,3));   		// 12 / 3    
+   		System.out.println(div(5,5));    		// 5 / 5  
+   		System.out.println(div(25,7));   		// 25 / 7
+   		System.out.println(mod(25,7));   		// 25 % 7
+   		System.out.println(mod(120,6));  		// 120 % 6    
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
@@ -25,43 +25,77 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int sum = 0;
+
+		for (int i = 0; i <= x2; i++) {
+			sum = x1++;
+		}
+		return sum;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int minus = 0;
+
+		for (int i = 0; i <= x2; i++) {
+			minus = x1--;
+		}
+		return minus;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int product = 0;
+
+		for (int i = 0; i < x2; i++) {
+			product = plus(product, x1);
+		}
+		return product;	
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int power = 1;
+
+		for (int i = 0; i <= n; i++) {
+			power = times(power, x);
+		}
+		return power;	
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int fullSub = 0;
+		int numerator = x1;
+		
+		while (numerator >= x2) {
+			numerator = minus(numerator, x2);
+			fullSub++;
+		}
+		return fullSub;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int fullSubs = div(x1, x2);
+		int fullNum = times(fullSubs, x2);
+		int remainder = minus(x1, fullNum);
+		
+		return remainder;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int square = 0;
+		int root = 0;
+		for (int i = 0; i < x; i++) {
+			square = times(i, i);
+
+			if (square >= x) {
+				root = i--;
+				break;
+			}
+		}
+		return root;
 	}	  	  
 }
